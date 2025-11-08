8b7be14a2276bde3d4273fb29a775e4eb842352a
samsamak <samsamabdik@outlook.com>
2025-11-08 13:13:25 +0100

    opg4

--- src/main/java/no/hvl/dat100/oppgave4/SkrivBlogg.java
+++ src/main/java/no/hvl/dat100/oppgave4/SkrivBlogg.java
@@ -2,14 +2,23 @@ package no.hvl.dat100.oppgave4;
 
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
+import java.io.File;
 
-import no.hvl.dat100.common.TODO;
 import no.hvl.dat100.oppgave3.*;
 
 public class SkrivBlogg {
 
 	public static boolean skriv(Blogg samling, String mappe, String filnavn) {
+            try {
+                PrintWriter skriver = new PrintWriter(mappe + "/" + filnavn);
+                skriver.print(samling.toString());
+                skriver.close();
 
-		throw new UnsupportedOperationException(TODO.method());
-	}
-}
+                return true;
+
+            } catch (FileNotFoundException e) {
+                return false;
+            }
+        }
+    }
+
